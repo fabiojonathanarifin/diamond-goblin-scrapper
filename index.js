c3c2e9fe0587a298acbb484656d01afc47808bb0
fabiojonathanarifin <fabiojonathanarifin@gmail.com>
2022-11-23 18:18:38 +0700

    redo code

--- index.js
+++ index.js
@@ -1,18 +1,62 @@
-const express = require("express");
+const PORT = 5000;
 const axios = require("axios");
 const cheerio = require("cheerio");
-const { data } = require("cheerio/lib/api/attributes");
-
+const express = require("express");
 const app = express();
 
-const PORT = 5000;
-app.listen(PORT, () => console.log(`server running on port ${3000}`));
+async function cryptopriceScraper() {
+  const url = "https://coinmarketcap.com/";
+  const coinArray = [];
+  await axios(url).then((response) => {
+    const html_data = response.data;
+    const $ = cheerio.load(html_data);
 
-const url = "https://coinmarketcap.com";
-axios(url).then((response) => {
-  const html_data = response.data;
-  const $ = cheerio.load(html_data);
+    const selectedElem =
+      "#__next > div.sc-c5c9d167-1.iZIMUr > div.main-content > div.sc-1a736df3-0.PimrZ.cmc-body-wrapper > div > div:nth-child(1) > div.sc-f7a61dda-2.efhsPu > table > tbody > tr:nth-child(5)";
+    const keys = [
+      "No.",
+      "Coin",
+      "Price",
+      "24h",
+      "7d",
+      "Marketcap",
+      "Volume",
+      "CirculatingSupply",
+    ];
+
+    $(selectedElem).each((parentIndex, parentElem) => {
+      let keyIndex = 0;
+      const coinDetails = {};
+      if (parentIndex <= 9) {
+        $(parentElem)
+          .children()
+          .each((childId, childElem) => {
+            const value = $(childElem).text();
+            if (value) {
+              coinDetails[keys[keyIndex]] = value;
+              keyIndex++;
+            }
+          });
+        coinArray.push(coinDetails);
+      }
+    });
+  });
+  return coinArray;
+}
+
+app.get("/api/crypto", async (req, res) => {
+  try {
+    const crypto = await cryptopriceScraper();
+    return res.status(200).json({
+      result: crypto,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      err: err.toString(),
+    });
+  }
 });
 
-const selectedElem =
-  "#__next > div.sc-c5c9d167-1.iZIMUr > div.main-content > div.sc-1a736df3-0.PimrZ.cmc-body-wrapper > div > div:nth-child(1) > div.sc-f7a61dda-2.efhsPu > table > tbody > tr:nth-child(1)";
+app.listen(PORT, () =>
+  console.log(`The server is active and running on port ${PORT}`)
+);
